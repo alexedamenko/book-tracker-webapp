ad4fd8026d4af8437b1bac07359ddaabd4287f59
alexedamenko <alexedamenko@gmail.com>
2025-08-01 22:13:56 +1000

    Update app.js

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ async function renderMainScreen() {
     </div>
 
     <div class="footer-buttons">
-      <button id="exportBtn">📤 Экспорт</button>
+      <button id="exportBtn">📤Export</button>
       <div id="formatMenu" class="hidden">
         <div class="format-option" data-format="csv">CSV</div>
         <div class="format-option" data-format="json">JSON</div>
