a12412ffe9272e3f45c29ab5ea5c0721e30da392
alexedamenko <alexedamenko@gmail.com>
2025-08-07 21:33:21 +1000

    Update uploadCover.js

--- api/uploadCover.js
+++ api/uploadCover.js
@@ -36,10 +36,9 @@ export default async function handler(req, res) {
       req.pipe(busboy);
     });
 
-    const ext = result.filename?.split('.').pop() || 'jpg';
-    const ext = (typeof result.filename === 'string' && result.filename.includes('.'))
-  ? result.filename.split('.').pop()
-  : 'jpg';
+    const ext = file.name.includes('.') ? file.name.split('.').pop() : 'jpg';
+    const fileName = `${crypto.randomUUID()}.${ext}`;
+
 
     const { error } = await supabase.storage
       .from("covers")
